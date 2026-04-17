c19ee3534f8f1d3e143254fd06b0e4266d010346
Fabricio Lima <fabriciotrab2005@gmail.com>
2026-04-17 20:08:06 -0300

    feat(backend): add nx nest libraries for auth, analytics, medical, and shared

--- backend/libs/infra/app.module.ts
+++ backend/libs/infra/app.module.ts
@@ -1,5 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
+import { AnalyticsModule } from '@healthflow/analytics';
+import { AuthModule } from '@healthflow/auth';
+import { MedicalModule } from '@healthflow/medical';
+import { SharedModule } from '@healthflow/shared';
 import { ConfigurationModule } from './configuration';
 import { ApiHealthCheck, DatabaseHealthCheck } from './health-check';
 import { HealthController } from './health-check/health.controller';
@@ -12,6 +16,10 @@ import { ProvidersModule } from '../providers/src/providers.module';
     PrismaModule,
     TerminusModule,
     ProvidersModule,
+    SharedModule,
+    AuthModule,
+    MedicalModule,
+    AnalyticsModule,
   ],
   controllers: [HealthController],
   providers: [ApiHealthCheck, DatabaseHealthCheck],
