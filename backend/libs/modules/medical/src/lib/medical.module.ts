b5d3e7dfccfd8cf2466efd32d7a7c2da52852ca9
Fabricio Lima <fabriciotrab2005@gmail.com>
2026-04-18 00:50:32 -0300

    chore(medical): register module providers

--- backend/libs/modules/medical/src/lib/medical.module.ts
+++ backend/libs/modules/medical/src/lib/medical.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ProvidersModule } from '@healthflow/providers';
+import { CreateReportUseCase } from './application/use-cases/create-report.use-case';
+import { ListExamsUseCase } from './application/use-cases/list-exams.use-case';
+import { UploadExamUseCase } from './application/use-cases/upload-exam.use-case';
+import { ExamProcessingQueuePort } from './domain/ports/exam-processing-queue.port';
+import { MedicalExamRepository } from './domain/repositories/medical-exam.repository';
+import { ExamConsumerService } from './application/messaging/exam-consumer.service';
+import { ExamProcessingQueueAdapter } from './application/messaging/exam-processing-queue.adapter';
+import { PrismaMedicalExamRepository } from './infrastructure/repositories/prisma-medical-exam.repository';
+import { ExamsController } from './presentation/http/controllers/exams.controller';
 
 @Module({
-	controllers: [],
-	providers: [],
-	exports: [],
+  imports: [ProvidersModule],
+  controllers: [ExamsController],
+  providers: [
+    ExamConsumerService,
+    { provide: MedicalExamRepository, useClass: PrismaMedicalExamRepository },
+    { provide: ExamProcessingQueuePort, useClass: ExamProcessingQueueAdapter },
+    UploadExamUseCase,
+    ListExamsUseCase,
+    CreateReportUseCase,
+  ],
 })
 export class MedicalModule {}
